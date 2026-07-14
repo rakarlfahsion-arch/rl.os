0b15158f6390fd9e6e21342b218b20a9ce1c01dc
beeplan136-lang <beeplan136@gmail.com>
2026-07-14 19:59:25 +0700

    export SidebarItem type

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,7 @@
 export { Button } from "./components/Button"
 export { Card, CardHeader, CardTitle, CardContent } from "./components/Card"
 export { Input } from "./components/Input"
-export { Sidebar } from "./components/Sidebar"
+export { Sidebar, type SidebarItem } from "./components/Sidebar"
 export { Modal } from "./components/Modal"
 export { Table, type Column } from "./components/Table"
 export { Badge } from "./components/Badge"
